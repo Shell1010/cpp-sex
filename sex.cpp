21fb6792ad83816c97683ae182c410f601975ca3
Shell <72198971+Shell1010@users.noreply.github.com>
2023-05-19 22:28:32 +0100

    Create sex.cpp

---
+++ sex.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#define sex using
+#define sexsex namespace
+#define sexsexsex std
+#define x ;
+#define sexsexsexsex int
+#define sexsexsexsexsex main
+#define sexer (
+#define sexman )
+#define sexless {
+#define sexy }
+#define sexsexsexsexsexsex cout
+#define sexsexsexsexsexsexsex <<
+#define sexballs =
+#define sexyvar 69
+#define sexsexsexsexsexsexsexsex return
+#define sexsexer while
+#define sexlessman "sex :"
+#define sexfulman "\n"
+#define sexsexar true
+#define sexway 0
+sex sexsex sexsexsex x
+sexsexsexsex sexsexsexsexsex sexer sexman sexless
+  sexsexsexsex sexvar sexballs sexyvar x
+  sexsexer sexer sexsexar sexman sexless
+    sexsexsexsexsexsex sexsexsexsexsexsexsex sexlessman sexsexsexsexsexsexsex sexyvar sexsexsexsexsexsexsex sexfulman x
+  sexy
+  sexsexsexsexsexsexsexsex sexway x 
+sexy
